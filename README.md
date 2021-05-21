abefae1cfc5a35685aa8cec0a6360fe18e40b7b6
Max B <max@radiallogic.co.uk>
2021-05-22 00:37:33 +0100

    bitz

---
+++ README.md
@@ -0,0 +1,10 @@
+
+To start server
+
+cd server
+npm start
+
+
+To compile client 
+
+yarn webpack
--- bootstrap.sh
+++ bootstrap.sh
@@ -19,7 +19,7 @@ sudo apt-get install dirmngr -y --install-recommends
 
 echo "Install nodejs "
 sudo apt-get install curl software-properties-common
-curl -sL https://deb.nodesource.com/setup_12.x | sudo bash -
+curl -sL https://deb.nodesource.com/setup_16.x | sudo bash -
 sudo apt-get install -y nodejs apache2 
 
 
